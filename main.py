69b381bfe6b84c415eab59a231583c033d482afd
Xuanye Li <xuan.yeah.li@gmail.com>
2024-05-01 15:40:21 -0400

    kjlk

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ def main():
 
     
     # Load the TensorFlow Lite model
-    interpreter = tflite.Interpreter(model_path="ei_audio.lite")
+    interpreter = tflite.Interpreter(model_path="ei_danger.lite")
     interpreter.allocate_tensors()
 
     while True:
